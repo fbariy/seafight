98f896d7c0c391f43e37053c702192fdaed22ecc
fbariy <bariy.alex@gmail.com>
2021-10-28 02:25:26 +0300

    Рефакторинг создания инвайта

--- src/main/scala/fbariy/seafight/application/invite/CreateInviteHandler.scala
+++ src/main/scala/fbariy/seafight/application/invite/CreateInviteHandler.scala
@@ -1,7 +1,7 @@
 package fbariy.seafight.application.invite
 
 import cats.data.Validated.{Invalid, Valid}
-import cats.data.{Validated, ValidatedNec}
+import cats.data.ValidatedNec
 import cats.effect.Sync
 import cats.implicits._
 import fbariy.seafight.application.error._
@@ -11,25 +11,25 @@ import java.util.UUID
 
 class CreateInviteHandler[F[_]: Sync](validator: CreateInviteValidator,
                                       inviteRepo: InviteRepository[F]) {
-  def handle(input: CreateInviteInput)
-    : F[ValidatedNec[AppError, InviteOutput]] = {
+  def handle(
+      input: CreateInviteInput): F[ValidatedNec[AppError, InviteOutput]] = {
     val trimmedInput = CreateInviteInput(Player(input.player1.name.trim),
                                          Player(input.player2.name.trim))
     import trimmedInput._
 
-    Sync[F]
-      .delay(
-        validator.playerIsNotEmpty(player1) |+|
-          validator.playerIsNotEmpty(player2) |+|
-          validator.playersAreNotSame(player1, player2)
-      )
-      .flatMap {
-        case Valid(_) =>
-          for {
-            id     <- Sync[F].delay(UUID.randomUUID())
-            invite <- inviteRepo.add(Invite(id, player1, player2))
-          } yield Valid(InviteOutput(invite))
-        case Invalid(e) => e.invalid[InviteOutput].pure[F]
+    for {
+      validationRes <- Sync[F].delay(validate(player1, player2))
+      result <- validationRes.traverse { _ =>
+        for {
+          id     <- Sync[F].delay(UUID.randomUUID())
+          invite <- inviteRepo.add(Invite(id, player1, player2))
+        } yield InviteOutput(invite)
       }
+    } yield result
   }
+
+  private def validate(p1: Player, p2: Player): ValidatedNec[AppError, Unit] =
+    validator.playerIsNotEmpty(p1) |+|
+      validator.playerIsNotEmpty(p2) |+|
+      validator.playersAreNotSame(p1, p2)
 }
